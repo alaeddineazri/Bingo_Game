571d21fe7d84875adb6531b3dcdec49c58614092
AlaeddineAzri <alaeddinazri@gmail.com>
2022-05-01 21:32:22 +0100

    first commit

---
+++ src/data/winCombination.js
@@ -0,0 +1,17 @@
+const Array1 = [0, 1, 2, 3, 4];
+const Array2 = [5, 6, 7, 8, 9];
+const Array3 = [10, 11, 12, 13, 14];
+const Array4 = [15, 16, 17, 18, 19];
+const Array5 = [20, 21, 22, 23, 24];
+const Array6 = [0, 5, 10, 15, 20];
+const Array7 = [1, 6, 11, 16, 21];
+const Array8 = [2, 7, 12, 17, 22];
+const Array9 = [3, 8, 13, 18, 23];
+const Array10 = [4, 9, 14, 19, 24];
+const Array11 = [0, 6, 12, 18, 24];
+const Array12 = [4, 8, 12, 16, 20];
+
+const winCombination = [Array1, Array2, Array3, Array4, Array5, Array6, Array7, Array8, Array9, Array10, Array11, Array12]
+
+
+export default winCombination
